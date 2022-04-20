5e70d1c2242b61cc6728174ac8afe901edf390d4
rafa30052003 <91888240+rafa30052003@users.noreply.github.com>
2022-04-20 14:48:38 +0200

    vista principal

---
+++ src/main/java/view/UI.java
@@ -0,0 +1,28 @@
+package view;
+
+public class UI {
+	
+	
+	
+	public void menuInicio() {
+		System.out.println("NUEVO USUARIO");
+		System.out.println("CONTINUAR CON TU USUARIO");
+	}
+	
+	public void menuPrincipal() {
+		System.out.println("_________________________NBA CODERE_________________________ ");
+		System.out.println("                                                            |");
+		System.out.println("(aqui voy a poner el dinero)                                |");
+		System.out.println("                                                            |");
+		System.out.println("                     [1] -> Equipo                          |");
+		System.out.println("                     [2] -> Estadisticas                    |");
+		System.out.println("                     [3] -> Mercado                         |");
+		System.out.println("                     [4] -> Estadisticas                    |");
+		System.out.println("                     [5] -> Salir                           |");
+		System.out.println("                                                            |");
+		System.out.println("____________________________________________________________|");
+		
+		
+		
+	}
+}
